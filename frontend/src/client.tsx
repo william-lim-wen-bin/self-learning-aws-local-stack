7ecf3b32a3e2cc0eef48ab317775a78b6164bbfd
wilim-seek <wilim@seekasia.com>
2026-06-04 09:50:42 +0800

    [frontend] Remove unnecessary comment

--- frontend/src/client.tsx
+++ frontend/src/client.tsx
@@ -4,7 +4,6 @@ import { App } from './components/App';
 import type { ClientContext } from './types';
 
 export default ({ environment }: ClientContext) => {
-  console.log({ environment, url: process.env.API_URL });
   hydrateRoot(
     document.getElementById('app')!,
     <App environment={environment} />,
